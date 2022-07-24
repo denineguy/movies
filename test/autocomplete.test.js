c444f56a2f0734f439ae43eba53bc4ec9d56d122
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-24 09:29:07 -0400

    adding assertions

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -12,5 +12,9 @@ it('Shows an autocomplete', () => {
     renderOption(movie) {
       return movie.Title;
     }
-  })
+  });
+
+  const dropdown = document.querySelector('.dropdown');
+
+  expect(dropdown.className).not.to.include('is-active');
 });
