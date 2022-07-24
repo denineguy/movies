a77cb26e3f9ee44b9425228f24878c32b76b7c01
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-24 09:19:07 -0400

    setting up the autocomplete

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,16 @@
 
-it('works!!', () => {
-
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData() {
+      return [
+        {Title: 'Avengers'},
+        {Title: 'Not Avengers'},
+        {Title: 'Some other movie'},
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  })
 });
